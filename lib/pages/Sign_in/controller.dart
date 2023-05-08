5a85bdb26335e1b7e568401e31d2e184f57c6b79
HUANG XAUNHUI <huangxh8531@gmail.com>
2023-05-08 10:58:01 +0800

    add import package and connect function

--- lib/pages/Sign_in/controller.dart
+++ lib/pages/Sign_in/controller.dart
@@ -3,56 +3,63 @@ import 'package:get/get.dart';
 import 'package:noodle/common/routes/routes.dart';
 import 'package:noodle/common/value/ColorStandar.dart';
 import 'package:noodle/pages/Sign_in/state.dart';
-class SignInController extends GetxController{
+import 'package:neo4driver/neo4driver.dart';
+
+class SignInController extends GetxController {
   SignInController();
   //some data
-  
-  final String back_riv="assets/animation_riv/background.riv";
-  final String first_riv="assets/animation_riv/cup_walk.riv";
 
+  final String back_riv = "assets/animation_riv/background.riv";
+  final String first_riv = "assets/animation_riv/cup_walk.riv";
 
   //controller
   TextEditingController NicknameController = TextEditingController();
   TextEditingController WhisperController = TextEditingController();
   // datetime
-  var Birthday=DateTime.now().obs;
-  void ChooseDate(DateTime pickdate) async{
-    if(pickdate!=Birthday.value){
+  var Birthday = DateTime.now().obs;
+  void ChooseDate(DateTime pickdate) async {
+    if (pickdate != Birthday.value) {
       print("finish");
-      Birthday.value=pickdate;
+      Birthday.value = pickdate;
       print(Birthday.value);
     }
   }
+
   //check info
-  void checkData(){
-    if(NicknameController.text.isNotEmpty && Birthday.value.isBefore(DateTime.now()) && WhisperController.text.isNotEmpty){
+  void checkData() {
+    if (NicknameController.text.isNotEmpty &&
+        Birthday.value.isBefore(DateTime.now()) &&
+        WhisperController.text.isNotEmpty) {
       print("ok info complete");
       Get.offAllNamed((AppRoutes.Home));
       //database connect
-    }
-    else{
+      NeoClient.withAuthorization(
+        username: 'neo4j',
+        password: 'EANSpNiLGgnXTPj8OTQE9HSXJ0b4a2sIVU2b6D44KJs',
+        databaseAddress: 'neo4j+s://73b8649a.databases.neo4j.io',
+        databaseName: 'Instance01',
+      );
+    } else {
       //info not complete
       Get.snackbar(
-        "Sing Up", 
+        "Sing Up",
         "Please complete info!",
         borderRadius: 20,
         icon: const Icon(Icons.person, color: Colors.white),
         margin: const EdgeInsets.all(30),
         isDismissible: true,
-        dismissDirection:DismissDirection.horizontal,
+        dismissDirection: DismissDirection.horizontal,
         colorText: AppColors.SecondText,
         backgroundColor: AppColors.Haveyorange,
         forwardAnimationCurve: Curves.easeOutBack,
         duration: const Duration(seconds: 3),
       );
     }
-
   }
 
-  
   final state = SignInState();
   @override
-  void onReady(){
+  void onReady() {
     super.onReady();
     print("SignIncontroller");
   }
